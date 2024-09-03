fa7110d7574a28bd2dc8255abfeca90a1081cfda
hollali <dheztinykartel@gmail.com>
2024-09-03 23:24:56 +0000

    "Added DoughnutChart component to TotalBalanceBox, updated package-lock.json and package.json to include react-chartjs-2 dependency"

--- components/totalBalanceBox.tsx
+++ components/totalBalanceBox.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AnimatedCounter from './animatedCounter'
 import { formatAmount } from '@/lib/utils'
 import CountUp from 'react-countup'
+import DoughnutChart from './doughnutChart'
 const TotalBalanceBox = ({
     accounts=[], totalBanks, totalCurrentBalance
 }:TotlaBalanceBoxProps) => {
@@ -9,16 +10,16 @@ const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/**  DoughnutChart **/}
+            <DoughnutChart accounts={accounts}/> 
         </div>
         <div className='flex flex-col gap-6'>
             <h2 className='header-2'>
                 Bank Accounts: {totalBanks} 
             </h2>
             <div className='flex flex-col gap-2'>
-                <span className='total-balance-label'>
+                <p className='total-balance-label'>
                     Total Current Balance
-                </span>
+                </p>
 
                 <div className='total-balance-amount flex-center gap-2'>
                     <AnimatedCounter amount=
