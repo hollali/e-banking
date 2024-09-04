5030c73c7b37176df6df76c5db2e831f25ab55d7
hollali <dheztinykartel@gmail.com>
2024-09-04 18:38:04 +0000

    "Added 'use client' directive, imported sidebarLinks and cn, and updated Sidebar component with new logo, links, and active link styling"

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,13 @@
+'use client';
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = ({user}:SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section>
         <nav className='flex flex-col gap-4'>
@@ -12,8 +17,20 @@ const Sidebar = ({user}:SiderbarProps) => {
             width={34}
             height={34}
             alt="Horizon logo"
+            className='size-[24px] max-xl:size-14'
             />
+            <h1 className='sidebar-logo'>Horizon</h1>
             </Link>
+            {sidebarLinks.map((item)=>{
+              const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+              return(
+                <Link href={item.route} key={item.label}
+                className={cn('sidebar-link',{'bg-bank-gradient':isActive})}
+                >
+                  {item.label}
+                </Link>
+              )
+            })}
         </nav>
     </section>
   )
