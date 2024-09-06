279464c67f8b325a25d0e9a3c6c45e6c73b2babe
hollali <dheztinykartel@gmail.com>
2024-09-06 12:40:06 +0000

    "Added AuthForm component to sign-in and sign-up pages"

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,11 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignUp = () => {
     return (
-        <div>Sign Up</div>
+        <section className="flex-center size-full max-sm:px-6">
+            <AuthForm type="sign-in"/>
+        </section>
     )
 }
 
---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthForm = ({type}: {type: string}) => {
+  return (
+    <div>{type}</div>
+  )
+}
+
+export default AuthForm
