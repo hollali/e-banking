ad9ad943726d4601afcf51c05cbe8fb7c754a66f
hollali <dheztinykartel@gmail.com>
2024-09-06 12:09:34 +0000

    "Updated bank data in Home component and added new elements to BankCard component"

---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div> Transactions</div>
+  )
+}
+
+export default TransactionHistory
--- components/bankCard.tsx
+++ components/bankCard.tsx
@@ -1,5 +1,6 @@
 import { formatAmount } from '@/lib/utils'
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const BankCard = ({account, userName,showBalance=true}:CreditCardProps) => {
@@ -15,8 +16,41 @@ const BankCard = ({account, userName,showBalance=true}:CreditCardProps) => {
                     {formatAmount(account.currentBalance)}
                 </p>
             </div>
+            <article className="flex flex-col gap-2">
+                <div className='flex justify-between'>
+                    <h1 className="text-12 font-semibold text-white">{userName}</h1>
+                    <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
+                </div>
+                <p className="text-14 font-semibold tracking-[1.1px] text-white">
+                ●●●● ●●●● ●●●●
+                    <span className='text-16'>1234</span>
+                </p>
+            </article>
         </div>
+        <div className="bank-card_icon">
+            <Image
+            src="/icons/Paypass.svg"
+            width={20}
+            height={24}
+            alt="pay"
+            />
+            <Image 
+            src="/icons/mastercard.svg"
+            width={45}
+            height={32}
+            alt="mastercard"
+            className="ml-5"
+            />
+        </div>
+        <Image
+        src="/icons/lines.png"
+        width={316}
+        height={190}
+        alt="lines"
+        className="absolute top-0 left-0"
+        />
         </Link>
+        {/*COPY*/}
     </div>
   )
 }
