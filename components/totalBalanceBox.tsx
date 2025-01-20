552f72318531b786808b474e9099cbe94bc74e92
hollali <dheztinykartel@gmail.com>
2025-01-20 14:41:23 +0000

    `Removed unnecessary line from TotalBalanceBox component`

--- components/totalBalanceBox.tsx
+++ components/totalBalanceBox.tsx
@@ -17,7 +17,6 @@ const TotalBalanceBox = ({
 				<h2 className="header-2">Bank Accounts: {totalBanks}</h2>
 				<div className="flex flex-col gap-2">
 					<p className="total-balance-label">Total Current Balance</p>
-
 					<div className="total-balance-amount flex-center gap-2">
 						<AnimatedCounter amount={totalCurrentBalance} />
 					</div>
