111e2c6ff3d6a5ca542a6e452e2d6430afd24cdf
hollali <dheztinykartel@gmail.com>
2025-05-26 19:07:03 +0000

    Refactor AuthForm and CustomInput components; update import paths and remove unused files

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import AuthForm from '@/components/AuthForm'
+import AuthForm from '@/components/authForm'
 import React from 'react'
 
 const SignUp = () => {
--- components/authForm.tsx
+++ components/authForm.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {Form,} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import CustomInput from "./CustomInput";
+import CustomInput from "./customInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
